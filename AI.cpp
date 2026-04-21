272e4c0717dfd5d3ac097768358a4d6654cf4e6e
maxgland <maxgland@umich.edu>
2026-04-21 00:29:57 -0400

    AI.cpp + Max functions

--- AI.cpp
+++ AI.cpp
@@ -24,3 +24,35 @@ string getAIPickupList(const Move& move, const BuildingState& buildingState,
                        const Floor& floorToPickup) {
     return "";
 }
+
+int getExplosionTime(const Person& p) {
+    int angerLevel = p.getAngerLevel();
+    int numFromMax = MAX_ANGER - angerLevel;
+    int ticks = numFromMax * TICKS_PER_ANGER_INCREASE;
+    
+    return ticks;
+}
+
+string getDominantDirection(const Floor& floor) {
+    string direction = "";
+    int upAngerSum = 0;
+    int downAngerSum = 0;
+    int numPeople = floor.getNumPeople();
+    for (int i = 0; i < numPeople; i++) {
+        if (floor.getPersonByIndex(i).getTargetFloor()
+            > floor.getPersonByIndex(i).getCurrentFloor()) {
+            upAngerSum += floor.getPersonByIndex(i).getAngerLevel();
+        }
+        else {
+            downAngerSum += floor.getPersonByIndex(i).getAngerLevel();
+        }
+    }
+    if (upAngerSum >= downAngerSum) {
+        direction = "up";
+    }
+    else {
+        direction = "down";
+    }
+    
+    return direction;
+}
